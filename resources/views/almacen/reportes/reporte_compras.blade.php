c02a285f6235310c8b8695d60a184d02071b8dd5
yessica luna <luna.rico.karla.yessica@gmail.com>
2020-03-09 14:01:57 -0600

    Modificación al reporte de compras de almacén

--- resources/views/almacen/reportes/reporte_compras.blade.php
+++ resources/views/almacen/reportes/reporte_compras.blade.php
@@ -1,11 +1,10 @@
 @extends('almacen.reportes.encabezado_reporte')
 @section('content')
 <thead>
-        <tr>
-          @foreach($headers as $header)
+       
+            @foreach($headers as $header)
               <th style="white-space: normal;">{{$header}}</th>
-          @endforeach
-        </tr>
+            @endforeach
 
 <tbody>        
 <!-- Foreach de Partida -->       
@@ -13,28 +12,29 @@
     @foreach ($articulos as $itemAr)
         @if($itemAr->id_cuenta == $itemPartida->id)
         <tr>
-            <td colspan="8"><hr></td>
+            <td colspan="12"></td>
         </tr>
         <tr>
             <th>Partida:</th>
             <th scope="row">{{$itemPartida->sscta}}</th>
-            <td colspan="4">{{$itemPartida->nombre}}</td>
+            <td colspan="4" style="font-weight: bold;">{{$itemPartida->nombre}}</td>
         </tr> 
         @break
         @endif
-    @endforeach              
+    @endforeach  
+
             <!-- Foreach de Articulos -->  
             @foreach ($articulos as $itemArticulo)
                 <!-- Comparación del id de cuenta del articulo con id de la partida -->  
                 @if ($itemArticulo->id_cuenta == $itemPartida->id)
                 <tr>
                     <td scope="row"> {{$itemArticulo->clave}} </td>
-                    <td>{{$itemArticulo->descripcion}}</td>
+                    <td> {{$itemArticulo->descripcion}}</td>
                     <td>{{$itemArticulo->no_factura}}</td>
                     <td>{{$itemArticulo->descripcion_corta}}</td>
-                    <td>{{$itemArticulo->cantidad}}</td>
-                    <td>{{$itemArticulo->precio_unitario}}</td>
-                    <td>{{$itemArticulo->subtotal}}</td>
+                    <td>{{ number_format($itemArticulo->cantidad) }}</td>
+                    <td>{{ number_format($itemArticulo->precio_unitario,2)}}</td>
+                    <td>{{ number_format($itemArticulo->subtotal,2) }}</td>
                 </tr>
 
                 @endif
@@ -43,21 +43,21 @@
         <!-- Contador de movimientos por partida -->
         @foreach($total_movimientos as $itemMovimientos) 
                 @if($itemMovimientos->sscta == $itemPartida->sscta )
-                    <th style="white-space: normal;">{{$itemMovimientos->count}} Movimientos</th>
+                    <th colspan="1" style="white-space: normal;">{{$itemMovimientos->count}} Movimientos</th>
                 @endif 
         @endforeach
 
         <!-- Suma de cantidades por partida -->
         @foreach($total_cantidades as $itemCantidades) 
                 @if($itemCantidades->sscta == $itemPartida->sscta )
-                    <th style="white-space: normal;">ARTÍCULOS: {{$itemCantidades->sum_cantidad}}</th>
+                    <th colspan="1" style="white-space: normal;">ARTÍCULOS: {{ number_format($itemCantidades->sum_cantidad) }}</th>
                 @endif 
         @endforeach
 
         <!-- Suma de subtotales por partida -->
         @foreach($total_subtotales as $itemSubtotales) 
                 @if($itemSubtotales->sscta == $itemPartida->sscta )
-                    <th style="white-space: normal;">SUBTOTAL: {{$itemSubtotales->sum_subtotal}}</th>
+                    <th colspan="2" style="white-space: normal;">SUBTOTAL: {{ number_format($itemSubtotales->sum_subtotal,2)}}</th>
                 @endif 
         @endforeach
 
@@ -66,20 +66,26 @@
          
 @endforeach
 
-<!-- Contador de movimientos generales -->
+
+<tr>
+    <td colspan="12"><hr></td>
+</tr>
+
+<!-- Contador de movimientos generales -->    
 @foreach($total_movimientos_general as $itemMovimientosGeneral) 
-    <th style="white-space: normal;">{{$itemMovimientosGeneral->count}} Movimientos</th>  
+    <td colspan="1"  style="font-size: 12px; font-weight: bold;">{{$itemMovimientosGeneral->count}} Movimientos</th>  
 @endforeach
 
 <!-- Suma de cantidades generales -->
 @foreach($total_cantidades_general as $itemCantidadesGeneral) 
-    <th style="white-space: normal;">ARTÍCULOS: {{$itemCantidadesGeneral->sum_cantidad}}</th>  
+    <td colspan="1"  style="font-size: 12px; font-weight: bold;">ARTÍCULOS: {{ number_format($itemCantidadesGeneral->sum_cantidad) }}</th>  
 @endforeach
 
 <!-- Suma de subtotales generales -->
 @foreach($total_subtotales_general as $itemSubtotalesGeneral) 
-    <th style="white-space: normal;">TOTAL: {{$itemSubtotalesGeneral->sum_subtotal}}</th>  
+    <td colspan="2"  style="font-size: 12px; font-weight: bold;">TOTAL: {{ number_format($itemSubtotalesGeneral->sum_subtotal,2) }}</th>  
 @endforeach
+
 
 @endsection
 
